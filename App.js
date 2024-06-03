0d2c4410fed7e731c537a039d582525b066d986c
Jonathan <andrejonathankarl@gmail.com>
2024-06-03 14:50:27 +0200

    Nouveaux changements

--- App.js
+++ App.js
@@ -1,18 +1,6 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-} from 'react-native';
-import Menu from './Menu';
-import Connexion from './Connexion';
-import EntrezCours from './EntrezCours';
-import FormatSelection from './FormatSelection';
-import { useNavigation } from '@react-navigation/native';
 import { NavigationContainer } from '@react-navigation/native';
-import Navigation from './Navigation'; 
+import Navigation from './Navigation';
 
 const App = () => {
   return (
